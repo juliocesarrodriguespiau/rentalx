a93434f67c9af3e47cf92cbdbf0fbec634d9ca55
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2022-06-15 22:58:54 -0300

    commit categories path

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -6,7 +6,7 @@ const categoriesRoutes = Router();
 
 const categories: Category[] = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
     const category = new Category();
