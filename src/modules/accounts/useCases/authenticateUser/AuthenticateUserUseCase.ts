1afdc83902cee59a8609c27e055a383fa953a0a2
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2023-01-28 12:43:54 -0300

    commit-user-token-jwt

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { compare } from "bcryptjs";
+import { inject, injectable } from "tsyringe";
+import { sign } from "jsonwebtoken";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string,
+        email: string,
+    },
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ){}
+    
+    async execute({email, password}: IRequest): Promise<IResponse> {
+    // usuário existe
+    const user = await this.usersRepository.findByEmail(email);
+
+    if(!user) {
+        throw new Error("Email or Password incorrect!");
+    }
+    // senha está correta
+    const passwordMatch = await compare(password, user.password);
+
+    if(!passwordMatch) {
+        throw new Error("Email or Password incorrect!");
+    }
+    // gerar webtoken // chave token md5 julioignite
+    const token = sign({}, "1b266bb9c4ed56b3a37c9c0def0fb4e1", {
+        subject: user.id,
+        expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+        token,
+        user: {
+            name: user.name,
+            email: user.email,
+        },
+    };
+
+    return tokenReturn;
+   }
+}  
+
+export { AuthenticateUserUseCase };
